c7611ac4bf0601405b6f83a3da137d75aab29169
Nayereh Afyoui <88449572+nafiuny@users.noreply.github.com>
2024-01-23 21:02:48 +0330

    Update icr_cyclegan_test.sh

--- bash_scripts/icr_cyclegan_test.sh
+++ bash_scripts/icr_cyclegan_test.sh
@@ -1,10 +1,10 @@
 python -m icr_cyclegan_vc.test \
-    --name icr_cyclegan_vc_VCC2SF2_VCC2TF1 \
+    --name icr_cyclegan_vc_VCC2SF2_VCC2SF1 \
     --save_dir results/ \
     --preprocessed_data_dir vcc2018_preprocessed/vcc2018_evaluation \
     --gpu_ids 0 \
     --speaker_A_id VCC2SF2 \
-    --speaker_B_id VCC2TF1 \
-    --ckpt_dir results/icr_cyclegan_vc_VCC2SF2_VCC2TF1/ckpts \
+    --speaker_B_id VCC2SF1 \
+    --ckpt_dir results/icr_cyclegan_vc_VCC2SF2_VCC2SF1/ckpts \
     --load_epoch 500 \
     --model_name generator_A2B \
